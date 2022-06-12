67d2fe2dd807ff1006cdfc690e6d4bd1e318603b
jonathan-durbin <jongdurbin@gmail.com>
2022-06-12 18:27:05 -0400

    Move some functions to util file

---
+++ util.py
@@ -0,0 +1,45 @@
+import datetime as dt
+
+def timestamp_to_datetime(t):
+    return dt.datetime.fromtimestamp(t).isoformat()
+
+def user_is_removed(redditor):
+    if (
+        (hasattr(redditor, 'is_blocked') and redditor.is_blocked == True)
+        or
+        (hasattr(redditor, 'is_suspended') and redditor.is_suspended == True)
+        and
+        hasattr(redditor, 'name')
+    ):
+        print(f'\n######## {redditor.name} IS A BLOCKED/SUSPENDED ACCOUNT ##########\n')
+        return True
+
+    elif hasattr(redditor, 'name') != True:  # Could also do `redditor is None`
+        print(f'\n######## FOUND ACCOUNT THAT WAS REMOVED ##########\n')
+        return True
+
+    else:
+        return False
+
+def update_readme(conn, path_to_file):
+    with open(path_to_file, 'r') as f:
+        lines = f.readlines()
+
+    total_posts = conn.execute('select count(*) from post').fetchall()[0][0]
+    total_accounts = conn.execute('select count(*) from user').fetchall()[0][0]
+
+    lines[7] = f'{total_posts:,} Posts\n'
+    lines[8] = f'{total_accounts:,} Accounts\n'
+
+    with open(path_to_file, 'w') as f:
+        f.writelines(lines)
+
+def progress_bar(items, item, fill, length):
+    # assumes a list with unique elements
+    i = items.index(item)
+
+    progress = 1-(len(items)-(i+1))/len(items)
+    fill_amount = round((length-2) * progress)
+    space = length - fill_amount
+
+    return f'[{fill * fill_amount}{" " * space}]'
